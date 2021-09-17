b7a97c817a7336c8375b8d537b40dfa476594cb8
alexflen <77319251+alexflen@users.noreply.github.com>
2021-09-17 11:41:39 +0500

    Обработан ввод (из параметров компиляции или командной строки) Добавлены сообщения об ошибках Добавлены объявления некоторых функций, которые будут использованы в программе

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,68 @@
-fun main(args: Array<String>) {
+import java.io.File
+
+fun terminate() {
     TODO()
+}
+
+fun errNotEnoughArgs() {
+    println("Недостаточно параметров для вызова функции")
+    terminate()
+}
+
+fun errUnknownOption(opt: Char) {
+    println("Неизвестная опция $opt")
+    terminate()
+}
+
+fun errWrongInputFile(fileName: String) {
+    println("Неверное имя файла: $fileName")
+    terminate()
+}
+
+fun printHelp() {
+    TODO()
+}
+
+fun parseInput(): Array<String> {
+    var input = readLine()
+    if (input != null) {
+        input += ' '    // чтобы последняя строка корректно обработалась
+        var tempStr = ""
+        var result = emptyArray<String>()
+        for (symb in input) {
+            if (symb == ' ') {
+                if (tempStr.isNotEmpty()) {
+                    result += tempStr
+                    tempStr = ""
+                }
+            } else {
+                tempStr += symb
+            }
+        }
+        return result
+    } else {
+        errNotEnoughArgs()
+        return emptyArray()
+    }
+}
+
+
+fun processCommand(commandList: Array<String>) {
+    TODO()
+}
+
+fun outputAnswer(showOpts: Int, hideOpts: Int, ignoreLines: Boolean, outputFile: String?) {
+    TODO()
+}
+
+// file1.txt file2.txt -s=uad -h=ins -i -w=answer.txt
+
+fun main(args: Array<String>) {
+    val commandList: Array<String> = if (args.isNotEmpty()) {
+        args.copyOf()
+    } else {
+        parseInput()
+    }
+
+
 }
